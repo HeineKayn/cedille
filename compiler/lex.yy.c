ce9fb89ef9bf25b3419974fb1179e0058fd1f283
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2022-03-10 09:31:50 +0100

    TFUNC to fix

--- compiler/lex.yy.c
+++ compiler/lex.yy.c
@@ -433,8 +433,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 /* %% [3.0] code to copy yytext_ptr to yytext[] goes here, if %array \ */\
 	(yy_c_buf_p) = yy_cp;
 /* %% [4.0] data tables for the DFA and the user's section 1 definitions go here */
-#define YY_NUM_RULES 23
-#define YY_END_OF_BUFFER 24
+#define YY_NUM_RULES 22
+#define YY_END_OF_BUFFER 23
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -442,14 +442,13 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[52] =
+static const flex_int16_t yy_accept[46] =
     {   0,
-        0,    0,   24,   22,   21,   21,   20,   16,   17,   14,
-       13,   10,   12,   15,    8,    9,   11,    7,    7,    7,
-        7,    7,    7,   18,   19,   21,    8,    0,    7,    7,
-        2,    7,    7,    7,    7,    0,    8,    7,    6,    7,
-        7,    7,    7,    1,    7,    7,    5,    7,    3,    4,
-        0
+        0,    0,   23,   21,   20,   20,   19,   15,   16,   13,
+       12,    9,   11,   14,    7,    8,   10,    6,    6,    6,
+        6,    6,   17,   18,   20,    7,    0,    6,    6,    2,
+        6,    6,    6,    0,    7,    6,    5,    6,    6,    6,
+        1,    6,    4,    3,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -466,8 +465,8 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,   16,    1,   17,   15,   18,   15,
 
        19,   20,   15,   21,   22,   15,   15,   23,   24,   25,
-       26,   27,   15,   28,   29,   30,   15,   15,   31,   15,
-       15,   15,   32,    1,   33,    1,    1,    1,    1,    1,
+       26,   15,   15,   15,   27,   28,   15,   15,   29,   15,
+       15,   15,   30,    1,   31,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -484,64 +483,60 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[34] =
+static const YY_CHAR yy_meta[32] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    2,    1,    1,    2,    2,    2,    2,    2,    2,
-        2,    2,    2,    2,    2,    2,    2,    2,    2,    2,
-        2,    1,    1
+        2,    2,    2,    2,    2,    2,    2,    2,    2,    1,
+        1
     } ;
 
-static const flex_int16_t yy_base[53] =
+static const flex_int16_t yy_base[47] =
     {   0,
-        0,    0,   69,   70,   32,   34,   70,   70,   70,   70,
-       70,   70,   56,   70,   26,   70,   70,    0,   41,   19,
-       49,   37,   43,   70,   70,   38,   30,   36,    0,   38,
-        0,   32,   39,   38,   37,   46,   45,   27,    0,   30,
-       29,   30,   22,    0,   21,   31,    0,   27,    0,    0,
-       70,   41
+        0,    0,   62,   63,   30,   32,   63,   63,   63,   63,
+       63,   63,   49,   63,   24,   63,   63,    0,   34,   17,
+       42,   37,   63,   63,   36,   28,   34,    0,   32,    0,
+       28,   33,   32,   41,   40,   24,    0,   25,   26,   20,
+        0,   26,    0,    0,   63,   39
     } ;
 
-static const flex_int16_t yy_def[53] =
+static const flex_int16_t yy_def[47] =
     {   0,
-       51,    1,   51,   51,   51,   51,   51,   51,   51,   51,
-       51,   51,   51,   51,   51,   51,   51,   52,   52,   52,
-       52,   52,   52,   51,   51,   51,   51,   51,   52,   52,
-       52,   52,   52,   52,   52,   51,   51,   52,   52,   52,
-       52,   52,   52,   52,   52,   52,   52,   52,   52,   52,
-        0,   51
+       45,    1,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   46,   46,   46,
+       46,   46,   45,   45,   45,   45,   45,   46,   46,   46,
+       46,   46,   46,   45,   45,   46,   46,   46,   46,   46,
+       46,   46,   46,   46,    0,   45
     } ;
 
-static const flex_int16_t yy_nxt[104] =
+static const flex_int16_t yy_nxt[95] =
     {   0,
         4,    5,    6,    7,    8,    9,   10,   11,   12,   13,
        14,   15,   16,   17,   18,    4,   18,   19,   18,   18,
-       18,   20,   18,   21,   18,   18,   22,   18,   18,   18,
-       23,   24,   25,   26,   26,   26,   26,   27,   31,   26,
-       26,   27,   29,   32,   28,   36,   50,   37,   28,   49,
-       48,   47,   46,   45,   44,   43,   37,   37,   42,   41,
-       40,   39,   38,   35,   34,   33,   30,   27,   51,    3,
-       51,   51,   51,   51,   51,   51,   51,   51,   51,   51,
-       51,   51,   51,   51,   51,   51,   51,   51,   51,   51,
-       51,   51,   51,   51,   51,   51,   51,   51,   51,   51,
+       18,   20,   18,   21,   18,   18,   18,   18,   22,   23,
+       24,   25,   25,   25,   25,   26,   30,   25,   25,   26,
+       28,   31,   27,   34,   44,   35,   27,   43,   42,   41,
+       40,   35,   35,   39,   38,   37,   36,   33,   32,   29,
+       26,   45,    3,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45
 
-       51,   51,   51
     } ;
 
-static const flex_int16_t yy_chk[104] =
+static const flex_int16_t yy_chk[95] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    5,    5,    6,    6,   15,   20,   26,
-       26,   27,   52,   20,   15,   28,   48,   28,   27,   46,
-       45,   43,   42,   41,   40,   38,   37,   36,   35,   34,
-       33,   32,   30,   23,   22,   21,   19,   13,    3,   51,
-       51,   51,   51,   51,   51,   51,   51,   51,   51,   51,
-       51,   51,   51,   51,   51,   51,   51,   51,   51,   51,
-       51,   51,   51,   51,   51,   51,   51,   51,   51,   51,
+        1,    5,    5,    6,    6,   15,   20,   25,   25,   26,
+       46,   20,   15,   27,   42,   27,   26,   40,   39,   38,
+       36,   35,   34,   33,   32,   31,   29,   22,   21,   19,
+       13,    3,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45
 
-       51,   51,   51
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -550,11 +545,11 @@ static char *yy_last_accepting_cpos;
 extern int yy_flex_debug;
 int yy_flex_debug = 1;
 
-static const flex_int16_t yy_rule_linenum[23] =
+static const flex_int16_t yy_rule_linenum[22] =
     {   0,
-       11,   13,   14,   15,   17,   18,   22,   27,   32,   33,
+       11,   13,   14,   16,   17,   21,   26,   31,   32,   34,
        35,   36,   37,   38,   39,   40,   41,   42,   43,   44,
-       45,   47
+       46
     } ;
 
 /* The intent behind this definition is that it'll catch
@@ -570,8 +565,8 @@ char *yytext;
 #include <stdlib.h>
 #include <stdio.h>
 #include "cedille.tab.h"
-#line 573 "lex.yy.c"
-#line 574 "lex.yy.c"
+#line 568 "lex.yy.c"
+#line 569 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -854,7 +849,7 @@ YY_DECL
 #line 9 "cedille.l"
 
 
-#line 857 "lex.yy.c"
+#line 852 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -883,13 +878,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 52 )
+				if ( yy_current_state >= 46 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 70 );
+		while ( yy_base[yy_current_state] != 63 );
 
 yy_find_action:
 /* %% [10.0] code to find the action number goes here */
@@ -912,13 +907,13 @@ do_action:	/* This label is used only to access EOF actions. */
 			{
 			if ( yy_act == 0 )
 				fprintf( stderr, "--scanner backing up\n" );
-			else if ( yy_act < 23 )
+			else if ( yy_act < 22 )
 				fprintf( stderr, "--accepting rule at line %ld (\"%s\")\n",
 				         (long)yy_rule_linenum[yy_act], yytext );
-			else if ( yy_act == 23 )
+			else if ( yy_act == 22 )
 				fprintf( stderr, "--accepting default rule (\"%s\")\n",
 				         yytext );
-			else if ( yy_act == 24 )
+			else if ( yy_act == 23 )
 				fprintf( stderr, "--(end of buffer or a NUL)\n" );
 			else
 				fprintf( stderr, "--EOF (start condition %d)\n", YY_START );
@@ -951,114 +946,109 @@ YY_RULE_SETUP
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 15 "cedille.l"
-{ return tFUNC; }
+#line 16 "cedille.l"
+{ return tCONST; }
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
 #line 17 "cedille.l"
-{ return tCONST; }
-	YY_BREAK
-case 6:
-YY_RULE_SETUP
-#line 18 "cedille.l"
 {
 	return tINT; 
 }
 	YY_BREAK
-case 7:
+case 6:
 YY_RULE_SETUP
-#line 22 "cedille.l"
+#line 21 "cedille.l"
 {
 	yylval.var = yytext[0];
 	return tVAR;
 }
 	YY_BREAK
-case 8:
+case 7:
 YY_RULE_SETUP
-#line 27 "cedille.l"
+#line 26 "cedille.l"
 {
 	yylval.nb = strtold(yytext, NULL);
 	return tNB;
 }
 	YY_BREAK
+case 8:
+YY_RULE_SETUP
+#line 31 "cedille.l"
+{ return tSTOP; }
+	YY_BREAK
 case 9:
 YY_RULE_SETUP
 #line 32 "cedille.l"
-{ return tSTOP; }
+{ return tVIR; }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 33 "cedille.l"
-{ return tVIR; }
+#line 34 "cedille.l"
+{ return tEGAL; }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
 #line 35 "cedille.l"
-{ return tEGAL; }
+{ return tSOU; }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
 #line 36 "cedille.l"
-{ return tSOU; }
+{ return tADD; }
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
 #line 37 "cedille.l"
-{ return tADD; }
+{ return tMUL; }
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
 #line 38 "cedille.l"
-{ return tMUL; }
+{ return tDIV; }
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
 #line 39 "cedille.l"
-{ return tDIV; }
+{ return tPO; }
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
 #line 40 "cedille.l"
-{ return tPO; }
+{ return tPF; }
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
 #line 41 "cedille.l"
-{ return tPF; }
+{ return tCO; }
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
 #line 42 "cedille.l"
-{ return tCO; }
+{ return tCF; }
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
 #line 43 "cedille.l"
-{ return tCF; }
-	YY_BREAK
-case 20:
-YY_RULE_SETUP
-#line 44 "cedille.l"
 { return tNOT; }
 	YY_BREAK
-case 21:
-/* rule 21 can match eol */
+case 20:
+/* rule 20 can match eol */
 YY_RULE_SETUP
-#line 45 "cedille.l"
+#line 44 "cedille.l"
 {}
+	YY_BREAK
+case 21:
+YY_RULE_SETUP
+#line 46 "cedille.l"
+{ return tERROR; }
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
 #line 47 "cedille.l"
-{ return tERROR; }
-	YY_BREAK
-case 23:
-YY_RULE_SETUP
-#line 48 "cedille.l"
 ECHO;
 	YY_BREAK
-#line 1061 "lex.yy.c"
+#line 1051 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1378,7 +1368,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 52 )
+			if ( yy_current_state >= 46 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1411,11 +1401,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 52 )
+		if ( yy_current_state >= 46 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 51);
+	yy_is_jam = (yy_current_state == 45);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -2207,4 +2197,4 @@ void yyfree (void * ptr )
 
 /* %ok-for-header */
 
-#line 48 "cedille.l"
+#line 47 "cedille.l"
--- compiler/test.sh
+++ compiler/test.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 OK_FOLDER=Tests_ok
+ERROR_FILES=()
 
 for i in `ls $OK_FOLDER`
 do
-    echo "------------------Executing file $i------------------"
-    cat $OK_FOLDER/$i | ./cedille >> test_results
-    echo "$? -0"
+    echo "------------------Executing file $i-----------------------"
+    lol=$(cat $OK_FOLDER/$i | ./cedille 2>&1 | grep "syntax error" | wc -l)
+    if test $lol -eq 1
+    then
+        ERROR_FILES+=($i)
+    fi
+done
+echo "Syntax errors for $OK_FOLDER at : "
+for i in ${ERROR_FILES[*]}
+do
+    echo $i
 done

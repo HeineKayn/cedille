35c9c28cace39d980d9fff9d7487156f5cebab77
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2022-03-22 09:33:27 +0100

    Asm code done (?)

--- compiler/asm_code.h
+++ compiler/asm_code.h
@@ -1,16 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 enum Operation {
     MOV,ADD,MUL,SOU,DIV,COP,AFC,JMP,JMF,INF,SUP,EQU,PRI
 };
 
 typedef struct {
-    char opération;
-    int * resultat;
-    int * operande1;
-    int * operande2;
-    int * numeroInstruction;
+    char operation;
+    int resultat;
+    int operande1;
+    int operande2;
+    int numeroInstruction;
+    int condition;
 } asmInstruct;
 
-void addAsmInstruct(enum Operation operation,int * resultat,int * operande1,int * operande2);
+void addAsmInstruct(enum Operation operation,int nombreArgument,...);
+
+void printAsmTable();

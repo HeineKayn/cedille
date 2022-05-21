a83520c8c5eeb991bca5d8f40c739ffc7a5c7896
ToTTToR <vikinor007@gmail.com>
2022-05-21 14:56:48 +0200

    Added in header asmcond

--- compiler/header/asm_code.h
+++ compiler/header/asm_code.h
@@ -18,5 +18,6 @@ void init_asm_table();
 int addAsmInstruct(enum Operation operation,int nombreArgument,...);
 void editAsmIf(int,enum Operation, enum Cond);
 void printAsmTable();
+void editAsmCond(int adressif,enum Operation operation,enum Cond cond);
 
 #endif
